c7bc1cba5650e83f18383fe6b39f9ec67f9b65fd
Juan David Gaviria Correa <63322751+JuanDa237@users.noreply.github.com>
2026-02-05 14:53:28 -0500

    build(app): install shadcn

--- src/table/auto-table.tsx
+++ src/table/auto-table.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export interface AutoCrudTableProps<T> {
   schema: T;
 }
 
 export function AutoTable() {
-  return <div>AutoTable Component</div>;
+  return (
+    <div>
+      AutoTable Component <Button>Hello World!</Button>
+    </div>
+  );
 }
